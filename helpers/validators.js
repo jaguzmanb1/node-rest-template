bd1a56786e609ff1eee9ba8a9afdb6545a2e71f8
jhon.guzman <jhon.guzman@rappi.com>
2023-10-24 22:03:47 -0500

    email validation

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,3 +1,5 @@
+const User = require("../models/users")
+
 const validateAge = (age = 0) => {
     if (age < 18) {
         throw new Error("Must have more than 18 years")
@@ -6,6 +8,17 @@ const validateAge = (age = 0) => {
     return true
 }
 
+const validateEmail = async(email) => {
+    const user = await User.find({email})
+    
+    if (user) {
+        throw new Error("Email already exists")
+    }
+
+    return true
+}
+
 module.exports = {
-    validateAge
+    validateAge,
+    validateEmail
 }
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const { usuariosGet, usuariosPost } = require("../controllers/users")
 const { check } = require("express-validator")
 const verifyFields = require("../middlewares/db-validator")
-const { validateAge } = require("../helpers/validators")
+const { validateAge, validateEmail } = require("../helpers/validators")
 
 const userRouter = Router()
 
@@ -11,7 +11,7 @@ userRouter.get("/", usuariosGet)
 userRouter.post("/", [
     check("name", "Name is required").not().isEmpty(),
     check("age", "Age must be a valid value").custom(validateAge),
-    check("email", "Must be a valid email").isEmail(),
+    check("email", "Must be a valid email").custom(validateEmail),
     check("phoneNumber", "PhoneNumber is required").not().isEmpty(),
     check("password", "Minimum password lenght is 8 characters").isLength({min:8}),
     verifyFields,
